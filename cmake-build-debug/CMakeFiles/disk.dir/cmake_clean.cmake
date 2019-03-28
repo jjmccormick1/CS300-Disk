985aa512a64fd61b07172abd30b10fec8ebd422b
Unknown <jjmccormick@crimson.ua.edu>
2019-03-28 15:33:11 -0500

    Built in files

---
+++ cmake-build-debug/CMakeFiles/disk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/disk.dir/disk.c.o"
+  "disk.pdb"
+  "disk"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/disk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/gen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gen.dir/gen.c.o"
+  "gen.pdb"
+  "gen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
