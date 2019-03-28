28b7a3e7decf29ac39233fbd4fbeef23ee45b6a1
Unknown <jjmccormick@crimson.ua.edu>
2019-03-28 15:33:22 -0500

    Work on disk

--- disk.c
+++ disk.c
@@ -5,16 +5,16 @@
 //Loads the queue then process the queue over and over again
 //Keep doing until the file empties
 
-request q[10000];
+request * q[10000];
 int qi = 0;
 int time = 0;
 FILE * file;
 
-void enqueue(request in) {
+void enqueue(request * in) {
     q[qi++] = in;
 }
-request dequeue(int pos) {
-    request r = q[pos];
+request * dequeue(int pos) {
+    request * r = q[pos];
     q[pos] = q[--qi];
     return r;
 }
@@ -27,9 +27,9 @@ int load_q(int time) {//time tells us what time it is in disk time
     int proc;
     fscanf(file, "%d%d%d\n", &t, &loc, &proc);
     while(t <= time && feof(file)) {
-        request x;
-        x.loc = loc;
-        x.proc = proc;
+        request * x = malloc(sizeof(request));
+        x->loc = loc;
+        x->proc = proc;
         enqueue(x);
         fscanf(file, "%d%d%d\n", &t, &loc, &proc);
     }
